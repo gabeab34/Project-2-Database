77ee13ebe04f3b2bd81866bfebd062ab885c1a1d
John Evenden <johnthefightguy@gmail.com>
2022-08-09 16:10:00 -0400

    Created initial files, set up connection, downloaded dependencies

---
+++ models/user.js
@@ -0,0 +1,34 @@
+import { Model, DataTypes } from "sequelize";
+import sequelize from "../config/connection.js";
+import bcrypt from "bcrypt";
+
+class User extends Model {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "user",
+  }
+);
+
+module.exports = User;
