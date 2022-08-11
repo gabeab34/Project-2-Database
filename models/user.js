54579d3c1a58ea8a0faf14e084e62218f39e077e
John Evenden <johnthefightguy@gmail.com>
2022-08-10 20:03:28 -0400

    Fuckin' with shit

--- models/user.js
+++ models/user.js
@@ -2,7 +2,11 @@ import { Model, DataTypes } from "sequelize";
 import sequelize from "../config/connection.js";
 import bcrypt from "bcrypt";
 
-class User extends Model {}
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 User.init(
   {
@@ -17,12 +21,26 @@ User.init(
       allowNull: false,
       unique: true,
     },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
     password: {
       type: DataTypes.STRING,
       allowNull: false,
     },
   },
   {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
