0a5798a80a1fdeab1e13a24fd33faf12a735f5a0
JVelezFD <jvelez117@gmail.com>
2022-08-17 18:47:19 -0400

    Adding Event test view and code

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 const userRoutes = require("./user_route.js");
 // const eventRoutes = require("./event_route.js");
-const match_routes = require("./match_route.js");
+const event_routes = require("./event_route.js");
 
 router.use("/users", userRoutes);
-router.use("/match", match_routes);
+router.use("/event", event_routes);
 
 module.exports = router;
