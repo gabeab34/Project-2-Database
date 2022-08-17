f2684ce23ea0ff7699040223d32f4351615016f6
John Evenden <johnthefightguy@gmail.com>
2022-08-16 20:04:56 -0400

    clearing

--- controllers/api/user_route.js
+++ controllers/api/user_route.js
@@ -3,7 +3,7 @@ const { User } = require("../../models");
 
 router.post("/login", async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { tag: req.body.tag } });
+    const userData = await User.findOne({ where: { email: req.body.email } });
 
     if (!userData) {
       res.status(400).json({ message: "User not found" });
