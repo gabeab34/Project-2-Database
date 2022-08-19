92829502953b2cf39ee4a5fab6276fa0337607db
rtran1029 <103476583+rtran1029@users.noreply.github.com>
2022-08-19 16:47:48 -0400

    new gif

--- public/js/profile.js
+++ public/js/profile.js
@@ -20,3 +20,6 @@
 // document
 //   .querySelector(".profile-form")
 //   .addEventListener("submit", loginFormHandler);
+$('.search-button').click(function(){
+    $(this).parent().toggleClass('open');
+  });
