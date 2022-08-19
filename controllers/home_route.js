264c78df1be8723fe2a8956d09ea3bdf97e69ee4
John Evenden <johnthefightguy@gmail.com>
2022-08-19 17:37:27 -0400

    Backend for bio button

--- controllers/home_route.js
+++ controllers/home_route.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
-const { User } = require("../models");
+const { User, Event } = require("../models");
 const withAuth = require("../utils/auth");
 
 router.get("/", withAuth, async (req, res) => {
   try {
     const userData = await User.findAll({
       attributes: { exclude: ["password"] },
-      order: [["name", "ASC"]],
+      // order: [["name", "ASC"]],
     });
 
     const users = userData.map((project) => project.get({ plain: true }));
